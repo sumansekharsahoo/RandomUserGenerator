1c248051b89713f9770a29da75ab5dcfcae9da41
Suman Sekhar Sahoo <114160074+sumansekharsahoo@users.noreply.github.com>
2023-07-03 21:30:17 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/index.js"
+  ],
+  theme: {
+    colors:{
+      'yelow':'#FFFAD7',
+      'orng':'#FFE4A7',
+      'grn':'#1B9C85',
+      'lloliv':'#F7FFE5',
+      'loliv':'#E1ECC8',
+      'oliv':'#E3F2C1',
+      'megb':'#3b82f6',
+      'meglg':'#059669'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
